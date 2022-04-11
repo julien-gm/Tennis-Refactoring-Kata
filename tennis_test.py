8119b02eec290bccb1150f2b215cb7c3aef7176a
julien-gm <julien-gm@users.noreply.github.com>
2022-04-11 17:08:14 +0200

    flake8 + black

--- tennis_test.py
+++ tennis_test.py
@@ -6,8 +6,9 @@ from tennis_unittest import test_cases, play_game
 
 
 class TestTennis:
-
-    @pytest.mark.parametrize('p1_points p2_points score p1_name p2_name'.split(), test_cases)
+    @pytest.mark.parametrize(
+        "p1_points p2_points score p1_name p2_name".split(), test_cases
+    )
     def test_get_score_game(self, p1_points, p2_points, score, p1_name, p2_name):
         game = play_game(p1_points, p2_points, p1_name, p2_name)
         assert score == game.score()
