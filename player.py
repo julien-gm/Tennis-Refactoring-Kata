f723e9d1ca871713f05e62c6b9c467a21dd1e1e2
julien-gm <julien-gm@users.noreply.github.com>
2022-04-11 17:16:21 +0200

    Create player class

---
+++ player.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+class Player:
+    def __init__(self, player_name):
+        self.name = player_name
+        self.score = 0
--- tennis_game.py
+++ tennis_game.py
@@ -1,40 +1,41 @@
 # -*- coding: utf-8 -*-
+from player import Player
+
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1Name = player1_name
-        self.player2Name = player2_name
-        self.player1_score = 0
-        self.player2_score = 0
+        self.player1 = Player(player1_name)
+        self.player2 = Player(player2_name)
 
     def won_point(self, player_name):
-        if player_name == self.player1Name:
-            self.player1_score += 1
+        if player_name == self.player1.name:
+            self.player1.score += 1
         else:
-            self.player2_score += 1
+            self.player2.score += 1
 
     def score(self):
-        if (self.player1_score < 4 and self.player2_score < 4) and (
-            self.player1_score + self.player2_score < 6
+        if (self.player1.score < 4 and self.player2.score < 4) and (
+            self.player1.score + self.player2.score < 6
         ):
             # no player have scored more than 4 points and less than 6 points have been played
             score_value = ["Love", "Fifteen", "Thirty", "Forty"]
-            advantage_player = score_value[self.player1_score] + "-"
+            advantage_player = score_value[self.player1.score] + "-"
             return advantage_player + (
                 "All"
-                if (self.player1_score == self.player2_score)
-                else score_value[self.player2_score]
+                if (self.player1.score == self.player2.score)
+                else score_value[self.player2.score]
             )
         else:
-            if self.player1_score == self.player2_score:
+            if self.player1.score == self.player2.score:
                 return "Deuce"
             advantage_player = (
-                self.player1Name
-                if self.player1_score > self.player2_score
-                else self.player2Name
+                self.player1.name
+                if self.player1.score > self.player2.score
+                else self.player2.name
             )
             score = (
                 "Win for "
-                if abs(self.player1_score - self.player2_score) > 1
+                if abs(self.player1.score - self.player2.score) > 1
                 else "Advantage "
             )
             return score + advantage_player
