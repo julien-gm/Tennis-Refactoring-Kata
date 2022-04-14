a79ad5ab162dbcf96a1f36fa9608db4b776e10b3
ccharpentier <clement.charpentier@viaco.fr>
2022-04-14 14:12:19 +0200

    Jojo_steal

--- tennis.py
+++ tennis.py
@@ -13,12 +13,15 @@ class TennisGame:
         self.player2 = Player(player2Name)
 
     def won_point(self, playerName):
-        if playerName == self.player1.name:
-            self.player1.points += 1
-        elif playerName == self.player2.name:
-            self.player2.points += 1
-        else:
-            raise Exception(playerName + " is not playing")
+        if self.set_win: 
+            raise Exception('Game is over')
+        else:    
+            if playerName != self.player1.name and playerName != self.player2.name:
+                raise Exception(playerName + " is not playing")
+            elif playerName == self.player1.name:
+                self.player1.points += 1
+            else:
+                self.player2.points += 1
 
     def score(self):
         if (self.player1.points < 4 and self.player2.points < 4) and (
@@ -49,8 +52,8 @@ class TennisGame:
                 else "Win for " + s
             )
     
-    def game(self):
-        if (self.player1.points >= 4 and self.player2.points >= 4) and (
-            self.player1.points + self.player2.points > 6
-        ):
-            raise Exception("Game is over")
+    # def game(self):
+    #     if (self.player1.points >= 4 and self.player2.points >= 4) and (
+    #         self.player1.points + self.player2.points > 6
+    #     ):
+    #         raise Exception('Game is over')
