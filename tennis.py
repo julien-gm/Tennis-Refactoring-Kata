3a00a3789e315eb17ffee32327cb5ae7e1e84c99
marianneSimplon <marianne.depierre.simplon@gmail.com>
2022-04-14 13:54:38 +0200

    Attribut win pour raiseExceptions

--- tennis.py
+++ tennis.py
@@ -11,38 +11,39 @@ class TennisGame:
     def __init__(self, player1Name, player2Name):
         self.player1 = Player(player1Name)
         self.player2 = Player(player2Name)
+        self.win = 0
 
     def won_point(self, playerName):
-        if playerName == self.player1.name:
+        if self.win == 1:
+            raise Exception('Game is over')
+        elif playerName == self.player1.name:
             self.player1.points += 1
-        else:
+        elif playerName == self.player2.name:
             self.player2.points += 1
+        elif playerName != self.player1.name or playerName != self.player2.name:
+            raise Exception(playerName+" is not playing")
 
     def score(self):
         if (self.player1.points < 4 and self.player2.points < 4) and (
             self.player1.points + self.player2.points < 6
         ):
             points = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = points[self.player1.points]
+            score = points[self.player1.points]
             return (
-                s + "-All"
+                score + "-All"
                 if (self.player1.points == self.player2.points)
-                else s + "-" + points[self.player2.points]
+                else score + "-" + points[self.player2.points]
             )
         else:
-            if self.player1.points == self.player2.points:
-                return "Deuce"
-            s = (
+            score = (
                 self.player1.name
                 if self.player1.points > self.player2.points
                 else self.player2.name
             )
-            return (
-                "Advantage " + s
-                if (
-                    (self.player1.points - self.player2.points)
-                    * (self.player1.points - self.player2.points)
-                    == 1
-                )
-                else "Win for " + s
-            )
+            if self.player1.points == self.player2.points:
+                return "Deuce"
+            elif ((self.player1.points - self.player2.points)*(self.player1.points - self.player2.points)) == 1:
+                return "Advantage " + score
+            else:
+                self.win = 1
+                return "Win for " + score
