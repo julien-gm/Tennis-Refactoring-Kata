e822842b25b4d113ba5abf64a1bc63eaa939d569
ccharpentier <clement.charpentier@viaco.fr>
2022-04-14 14:59:37 +0200

    afternoon_test2

--- tennis.py
+++ tennis.py
@@ -17,19 +17,15 @@ class TennisGame:
         if self.set_win == True: 
             raise Exception('Game is over')
         else:    
-            if playerName != self.player1.name and playerName != self.player2.name:
-                raise Exception(playerName + " is not playing")
-            elif playerName == self.player1.name:
+            if playerName == self.player1.name:
                 self.player1.points += 1
-            else:
+            elif:
                 self.player2.points += 1
+            else playerName != self.player1.name and playerName != self.player2.name:
+                raise Exception(playerName + " is not playing")
 
     def score(self):
-        if (self.player1.points >= 4 and self.player2.points >= 4) and (
-            self.player1.points + self.player2.points > 6
-        ):
-            self.set_win == True
-        elif (self.player1.points < 4 and self.player2.points < 4) and (
+        if (self.player1.points < 4 and self.player2.points < 4) and (
             self.player1.points + self.player2.points < 6
         ):
             points = ["Love", "Fifteen", "Thirty", "Forty"]
@@ -39,6 +35,10 @@ class TennisGame:
                 if (self.player1.points == self.player2.points)
                 else s + "-" + points[self.player2.points]
             )
+        elif (self.player1.points >= 4 and self.player2.points >= 4) and (
+        self.player1.points + self.player2.points > 6
+        ):
+            self.set_win == True
         else:
             if self.player1.points == self.player2.points:
                 return "Deuce"
