7a7cea491935faa11c804276632737ba4b6834ed
ellana <ellanaca1592@gmail.com>
2023-01-10 13:37:14 +0100

    modif name

--- tennis.py
+++ tennis.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class TennisGame1:
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
@@ -58,18 +59,18 @@ class TennisGame2:
 
     def won_point(self, playerName):
         if playerName == self.player1Name:
-            self.p1points += 1
+            self.P1Score()
         elif playerName == self.player2Name:
-            self.p2points += 1
+            self.P2Score()
 
     def score(self):
         result = ""
         if self.p1points == self.p2points and self.p1points < 3:
             if self.p1points == 0:
                 result = "Love"
-            elif self.p1points == 1:
+            if self.p1points == 1:
                 result = "Fifteen"
-            elif self.p1points == 2:
+            if self.p1points == 2:
                 result = "Thirty"
             result += "-All"
         if self.p1points == self.p2points and self.p1points > 2:
@@ -80,9 +81,9 @@ class TennisGame2:
         if self.p1points > 0 and self.p2points == 0:
             if self.p1points == 1:
                 P1res = "Fifteen"
-            elif self.p1points == 2:
+            if self.p1points == 2:
                 P1res = "Thirty"
-            elif self.p1points == 3:
+            if self.p1points == 3:
                 P1res = "Forty"
 
             P2res = "Love"
@@ -90,9 +91,9 @@ class TennisGame2:
         if self.p2points > 0 and self.p1points == 0:
             if self.p2points == 1:
                 P2res = "Fifteen"
-            elif self.p2points == 2:
+            if self.p2points == 2:
                 P2res = "Thirty"
-            elif self.p2points == 3:
+            if self.p2points == 3:
                 P2res = "Forty"
 
             P1res = "Love"
@@ -101,21 +102,21 @@ class TennisGame2:
         if self.p1points > self.p2points and self.p1points < 4:
             if self.p1points == 2:
                 P1res = "Thirty"
-            elif self.p1points == 3:
+            if self.p1points == 3:
                 P1res = "Forty"
             if self.p2points == 1:
                 P2res = "Fifteen"
-            elif self.p2points == 2:
+            if self.p2points == 2:
                 P2res = "Thirty"
             result = P1res + "-" + P2res
         if self.p2points > self.p1points and self.p2points < 4:
             if self.p2points == 2:
                 P2res = "Thirty"
-            elif self.p2points == 3:
+            if self.p2points == 3:
                 P2res = "Forty"
             if self.p1points == 1:
                 P1res = "Fifteen"
-            elif self.p1points == 2:
+            if self.p1points == 2:
                 P1res = "Thirty"
             result = P1res + "-" + P2res
 
@@ -139,32 +140,54 @@ class TennisGame2:
             result = "Win for " + self.player2Name
         return result
 
+    def SetP1Score(self, number):
+        for i in range(number):
+            self.P1Score()
+
+    def SetP2Score(self, number):
+        for i in range(number):
+            self.P2Score()
+
+    def P1Score(self):
+        self.p1points += 1
+
+    def P2Score(self):
+        self.p2points += 1
+
 
 class TennisGame3:
     def __init__(self, player1Name, player2Name):
-        self.p1N = player1Name
-        self.p2N = player2Name
-        self.p1 = 0
-        self.p2 = 0
+        self.name_player_1 = player1Name
+        self.name_player_2 = player2Name
+        self.score_player_1 = 0
+        self.score_player_2 = 0
 
-    def won_point(self, n):
-        if n == self.p1N:
-            self.p1 += 1
-        elif n == self.p2N:
-
-            self.p2 += 1
+    def won_point(self, name):
+        if name == self.name_player_1:
+            self.score_player_1 += 1
+        elif name == self.name_player_2:
+            self.score_player_2 += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+        if (self.score_player_1 < 4 and self.score_player_2 < 4) and (self.score_player_1 + self.score_player_2 < 6):
+            point = ["Love", "Fifteen", "Thirty", "Forty"]
+            real_score = point[self.score_player_1]
+            if (self.score_player_1 == self.score_player_2):
+                return real_score + "-All" 
+            else :
+                return real_score + "-" + point[self.score_player_2]
         else:
-            if self.p1 == self.p2:
+            
+            if self.score_player_1 == self.score_player_2:
                 return "Deuce"
-            s = self.p1N if self.p1 > self.p2 else self.p2N
-            return (
-                "Advantage " + s
-                if ((self.p1 - self.p2) * (self.p1 - self.p2) == 1)
-                else "Win for " + s
-            )
+            
+            if self.score_player_1 > self.score_player_2 :
+                win_player = self.name_player_1  
+            else:
+                win_player = self.name_player_2
+            
+            if abs(self.score_player_1 - self.score_player_2)  == 1:
+                return "Advantage " + win_player
+            else :
+                return  "Win for " + win_player
+            
